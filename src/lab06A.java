d279784e1d0a88d748e3a3863a7b2fe0dc886c37
herman888 <72478245+herman888@users.noreply.github.com>
2026-02-24 13:39:54 -0500

    Update lab06A.java with new random number changes

--- src/lab06A.java
+++ src/lab06A.java
@@ -1,5 +1,33 @@
+
+import java.util.Scanner;
+import java.util.Random;
+
+
 public class lab06A{
     public static void main(String[] args){
-        System.out.print("");
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.print(" Enter your upper range as a number");
+
+        int x = scanner.nextInt();
+
+        System.out.print("Enter your lower range as a number");
+
+        int y = scanner.nextInt();
+
+        int randomNumber = random.nextInt((x-y)+1) + y;
+
+
+        System.out.print(randomNumber);
+
+
+
+
+
+
+
+
     }
 }
